341a813d5f88496d9409ca913e2e501e03217846
Jason <jakiracofe10@gmail.com>
2016-12-09 17:32:37 -0600

    "add structs for point and rect"

--- main.cpp
+++ main.cpp
@@ -3,230 +3,43 @@
 
 using namespace std;
 
+struct point {
+    int x;
+    int y;
+    point(int x1,int y1) : x(x1), y(y1) {}
+};
 
-void findXAxisIntersections(int minx1,int maxx1,int miny1,int maxy1,int minx2,int maxx2,int miny2,int maxy2, vector<vector<int>> &results) {
-    vector<int> push;
-    //rec2 boundaries outside rec1
-    if (minx2 < minx1 && maxx2 > maxx1) {
-        if (miny2 > miny1 && maxy2 < maxy1) {
-            push.push_back(minx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-            push.push_back(minx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-        }
-        if (miny2 > miny1 && maxy2 > maxy1) {
-            push.push_back(minx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-        }
-        if (miny2 < miny1 && maxy2 < maxy1) {
-            push.push_back(minx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-        }
+struct rectangle {
+    point botLeft;
+    point topRight;
+    rectangle(point p1, point p2) : botLeft(p1), topRight(p2) {}
+};
+
+bool doRectanglesIntersect(rectangle r1, rectangle r2) {
+    int r1Width = r1.topRight.x-r1.botLeft.x;
+    int r1Height = r1.topRight.y-r1.botLeft.y;
+    int r2Width = r2.topRight.x-r2.botLeft.x;
+    int r2Height = r2.topRight.y-r2.botLeft.y;
+    bool result = true;
+    if (!(r1.botLeft.x > r2.topRight.x || r1.topRight.x < r2.botLeft.x)) {
+        return true;
+    } else if (!(r1.botLeft.y > r2.topRight.y || r1.topRight.y < r2.botLeft.y)) {
+        return true;
+    } else if (r1.botLeft.x > r2.botLeft.x && r1.topRight.x < r2.topRight.x) {
+        return true;
+    } else if (r1.botLeft.y > r2.botLeft.y && r1.topRight.y < r2.topRight.y) {
+        return true;
     }
-    //rec2 offset right
-    if (minx2 > minx1 && maxx2 > maxx1) {
-        if (miny2 > miny1 && maxy2 < maxy1) {
-            push.push_back(maxx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-        }
-        if (miny2 > miny1 && maxy2 > maxy1) {
-            push.push_back(maxx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-        }
-        if (miny2 < miny1 && maxy2 < maxy1) {
-            push.push_back(maxx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-        }
-    }
-    //rec2 offset left
-    if (minx2 < minx1 && maxx2 < maxx1) {
-        if (miny2 > miny1 && maxy2 < maxy1) {
-            push.push_back(minx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-            push.push_back(minx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-        }
-        if (miny2 > miny1 && maxy2 > maxy1) {
-            push.push_back(minx1);
-            push.push_back(miny2);
-            results.push_back(push);
-            push.clear();
-        }
-        if (miny2 < miny1 && maxy2 < maxy1) {
-            push.push_back(minx1);
-            push.push_back(maxy2);
-            results.push_back(push);
-            push.clear();
-        }
-    }
+    return false;
 }
 
-void findYAxisIntersections(int minx1,int maxx1,int miny1,int maxy1,int minx2,int maxx2,int miny2,int maxy2, vector<vector<int>> &results) {
-    vector<int> push;
-    //rec2 boundaries outside rec1
-    if (miny2 < miny1 && maxy2 > maxy1) {
-        if (minx2 > minx1 && maxx2 < maxx1) {
-            push.push_back(minx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-            push.push_back(minx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-        }
-        if (minx2 > minx1 && maxx2 > maxx1) {
-            push.push_back(minx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-            push.push_back(minx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-        }
-        if (minx2 < minx1 && maxx2 < maxx1) {
-            push.push_back(maxx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-        }
-    }
-    //rec2 offset up
-    if (miny2 > miny1 && maxy2 > maxy1) {
-        if (minx2 > minx1 && maxx2 < maxx1) {
-            push.push_back(minx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-        }
-        if (minx2 > minx1 && maxx2 > maxx1) {
-            push.push_back(minx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-        }
-        if (minx2 < minx1 && maxx2 < maxx1) {
-            push.push_back(minx2);
-            push.push_back(maxy1);
-            results.push_back(push);
-            push.clear();
-        }
-    }
-    //rec2 offset down
-    if (miny2 < miny1 && maxy2 < maxy1) {
-        if (minx2 > minx1 && maxy2 < maxy1) {
-            push.push_back(minx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-            push.push_back(maxx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-        }
-        if (minx2 > minx1 && maxx2 > maxx1) {
-            push.push_back(minx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-        }
-        if (minx2 < minx1 && maxx2 < maxx1) {
-            push.push_back(maxx2);
-            push.push_back(miny1);
-            results.push_back(push);
-            push.clear();
-        }
-    }
-}
-
-//lines must cross for there to be an intersection
-void findIntersections(vector<vector<int>> rec1, vector<vector<int>> rec2, vector<vector<int>> &results) {
-    int minx1 = rec1[0][0];
-    int maxx1 = rec1[2][0];
-    int miny1 = rec1[0][1];
-    int maxy1 = rec1[2][1];
-    int minx2 = rec2[0][0];
-    int maxx2 = rec2[2][0];
-    int miny2 = rec2[0][1];
-    int maxy2 = rec2[2][1];
-
-    findXAxisIntersections(minx1,maxx1,miny1,maxy1,minx2,maxx2,miny2,maxy2,results);
-    findYAxisIntersections(minx1,maxx1,miny1,maxy1,minx2,maxx2,miny2,maxy2,results);
-}
-
-void printResults(vector<vector<int>> &results) {
-    for (int i=0; i<results.size(); i++) {
-        cout << "(";
-        for (int j=0; j<results[0].size(); j++) {
-            cout << results[i][j];
-            if (j==0) {
-                cout <<",";
-            }
-        }
-        cout << ")";
-    }
-}
-
-//Rectangles written in x,y form, clock-wise from bottom left
 int main() {
-    vector<vector<int>> rec1 = {{1,1},{1,5},{5,5},{5,1}};
-    vector<vector<int>> rec2 = {{2,2},{2,6},{4,6},{4,2}};
-    vector<vector<int>> results;
-    findIntersections(rec1, rec2, results);
-    printResults(results);
+    point p1BL(0,0);
+    point p1TR(5,5);
+    point p2BL(2,2);
+    point p2TR(3,3);
+    rectangle r1(p1BL, p1TR);
+    rectangle r2(p2BL, p2TR);
+    bool result = doRectanglesIntersect(r1, r2);
+    cout << result;
 }
