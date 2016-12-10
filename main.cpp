5730c43d1901f40806806c51151b8a3ad8198e45
Jason <jakiracofe10@gmail.com>
2016-12-09 18:52:52 -0600

    "fix intersection logic"

--- main.cpp
+++ main.cpp
@@ -20,24 +20,25 @@ bool doRectanglesIntersect(rectangle r1, rectangle r2) {
     int r1Height = r1.topRight.y-r1.botLeft.y;
     int r2Width = r2.topRight.x-r2.botLeft.x;
     int r2Height = r2.topRight.y-r2.botLeft.y;
-    bool result = true;
-    if (!(r1.botLeft.x > r2.topRight.x || r1.topRight.x < r2.botLeft.x)) {
-        return true;
-    } else if (!(r1.botLeft.y > r2.topRight.y || r1.topRight.y < r2.botLeft.y)) {
-        return true;
-    } else if (r1.botLeft.x > r2.botLeft.x && r1.topRight.x < r2.topRight.x) {
-        return true;
-    } else if (r1.botLeft.y > r2.botLeft.y && r1.topRight.y < r2.topRight.y) {
-        return true;
+    if (r1.botLeft.x > r2.topRight.x || r1.topRight.x < r2.botLeft.x) {
+        return false;
     }
-    return false;
+    if (r1.botLeft.y > r2.topRight.y || r1.topRight.y < r2.botLeft.y) {
+        return false;
+    }
+    if (r1.botLeft.x < r2.botLeft.x && r1.topRight.x > r2.topRight.x && r1.botLeft.y < r2.botLeft.y && r1.topRight.y > r2.topRight.y) {
+        return false;
+    }
+    //add logic for both x being less than max x
+    //add logic for both y being less than max y
+    return true;
 }
 
 int main() {
     point p1BL(0,0);
     point p1TR(5,5);
     point p2BL(2,2);
-    point p2TR(3,3);
+    point p2TR(3,5);
     rectangle r1(p1BL, p1TR);
     rectangle r2(p2BL, p2TR);
     bool result = doRectanglesIntersect(r1, r2);
